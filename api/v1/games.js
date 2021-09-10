c0ee3bebad865e9a79b1e65d70d4fa0a913b3a01
Chris Fortier <dev@chrisjfortier.com>
2021-09-09 21:43:02 -0700

    make it so only an admin can edit scores

--- api/v1/games.js
+++ api/v1/games.js
@@ -6,15 +6,9 @@ const config = require("../../knexfile");
 const mysqldb = knex(config);
 const validate_jwt = require("../../utils/validate_jwt");
 
-// @route      GET api/v1/games (http://localhost:3060/api/v1/games)
-// @desc       this gets the games for a given season and week
-// @access     Private
-// test: http://localhost:3060/api/v1/games
-router.get("/", validate_jwt, (req, res) => {
-   const user_id = req.user.id; // get the user id from the JWT
+// this gets the games for a given season and week
+router.get("/", (req, res) => {
    const { season, week } = req.query; // grabbing variables from req.query
-
-   console.log(`finding games in season ${season} and week ${week}`);
 
    mysqldb
       .select()
@@ -30,26 +24,40 @@ router.get("/", validate_jwt, (req, res) => {
       });
 });
 
+// this updates the score of a single game
 router.patch("/update-score", validate_jwt, (req, res) => {
    const user_id = req.user.id; // get the user id from the JWT
    const { game_id, away_score, home_score } = req.query; // grabbing variables from req.query
 
-   // TODO: only allow an admin account to do this
-
-   mysqldb("games")
-      .where("id", game_id)
-      .update({
-         // this is so if you send empty values, it will put nulls into the db
-         away_score: away_score === "" ? null : away_score,
-         home_score: home_score === "" ? null : home_score,
-      })
-      .then(() => {
-         // return a success message
-         return res.status(200).json("game updated");
+   mysqldb
+      .select()
+      .from("users")
+      .where({ id: user_id })
+      .then((users) => {
+         if (users[0].is_admin === 1) {
+            // this user is an admin
+            mysqldb("games")
+               .where("id", game_id)
+               .update({
+                  // this is so if you send empty values, it will put nulls into the db
+                  away_score: away_score === "" ? null : away_score,
+                  home_score: home_score === "" ? null : home_score,
+               })
+               .then(() => {
+                  // return a success message
+                  return res.status(200).json("Game updated.");
+               })
+               .catch((err) => {
+                  // return the error
+                  return res.status(400).json("Error");
+               });
+         } else {
+            // this user is not an admin
+            return res.status(400).json("This user is not an admin.");
+         }
       })
       .catch((err) => {
-         // return the error
-         return res.status(400).json("error");
+         console.log("err", err);
       });
 });
 
--- client/src/components/pages/Results.js
+++ client/src/components/pages/Results.js
@@ -1,34 +1,44 @@
 import React, { useEffect, useState } from "react";
 import NavBar from "../ui/NavBar";
 import { connect } from "react-redux";
-// import classnames from "classnames";
 import { get_week_or_season_text } from "../../utils/helpers";
-// import toDisplayDate from "date-fns/format";
 import axios from "axios";
 import uuid from "uuid";
 
 function Results({ group_season_week }) {
    const [games, set_games] = useState([]);
+   const [message_from_server, set_message_from_server] = useState("");
+
    useEffect(() => {
       console.log("use effect");
+      set_message_from_server("Getting games from the server...");
       axios
-         // .get(
-         //    `/api/v1/picks?group_id=${group_season_week.group_id}&season=${group_season_week.season}&week=${group_season_week.week}`
-         // )
          .get(
             `/api/v1/games?season=${group_season_week.season}&week=${group_season_week.week}`
          )
          .then((res) => {
             console.log("res", res);
-            set_games(res.data);
+            set_games(
+               // initialize each game with a blank note
+               res.data.map((game) => {
+                  return { ...game, note: "" };
+               })
+            );
+            set_message_from_server(
+               `Received ${res.data.length} games from the server.`
+            );
          })
          .catch((err) => {
             const data = err.response.data;
             console.log("err", data);
 
             // todo: post some message
+            set_message_from_server(
+               "There was a problem getting games from the server."
+            );
          });
    }, [group_season_week]);
+
    return (
       <>
          <NavBar />
@@ -46,7 +56,8 @@ function Results({ group_season_week }) {
                   </h2>
                </div>
                <div className="card-body">
-                  {games.map((game) => {
+                  <p>{message_from_server}</p>
+                  {games.map((game, game_index) => {
                      return (
                         <p key={uuid.v4()}>
                            {game.away_team}{" "}
@@ -69,9 +80,6 @@ function Results({ group_season_week }) {
                               onClick={() => {
                                  console.log("clicked");
                                  axios
-                                    // .get(
-                                    //    `/api/v1/picks?group_id=${group_season_week.group_id}&season=${group_season_week.season}&week=${group_season_week.week}`
-                                    // )
                                     .patch(
                                        `/api/v1/games/update-score?game_id=${
                                           game.id
@@ -86,16 +94,23 @@ function Results({ group_season_week }) {
                                        }`
                                     )
                                     .then((res) => {
-                                       console.log("res", res);
+                                       // insert the response message into this game's note
+                                       const new_games = [...games];
+                                       new_games[game_index].note = res.data;
+                                       set_games(new_games);
                                     })
                                     .catch((err) => {
-                                       const data = err.response.data;
-                                       console.log("err", data);
+                                       // insert the response error into this game's note
+                                       const new_games = [...games];
+                                       new_games[game_index].note =
+                                          err.response.data;
+                                       set_games(new_games);
                                     });
                               }}
                            >
                               Update
-                           </button>
+                           </button>{" "}
+                           {game.note}
                         </p>
                      );
                   })}
