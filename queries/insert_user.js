2faa62c334c26dfe80d454f7bb1905ef4dc0d7bb
Chris Fortier <dev@chrisjfortier.com>
2022-12-06 08:26:48 -0800

    move files around more similar to next structure

--- queries/insert_user.js
+++
@@ -1,8 +0,0 @@
-// this creates a new user on the database
-
-// new syntax, with this method we just pass an object and the database will use it if all the keys match
-const insert_user = `
-   INSERT INTO users SET ?;
-`;
-
-module.exports = insert_user;
--- queries/select_game.js
+++
@@ -1,6 +0,0 @@
-// get the details of a single game form the game id
-const select_game = `
-   SELECT * from \`games\` where \`id\` = ?;
-`;
-
-module.exports = select_game;
