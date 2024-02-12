515f25024d1cf1bae0ef2c5eff38ec461a24964a
Chris Fortier <dev@chrisjfortier.com>
2024-02-11 19:55:06 -0800

    add 2023 medal

--- pages/standings.js
+++ pages/standings.js
@@ -8,6 +8,7 @@ const MEDALS = [
    { user_id: "ba1c83b1-9899-42e0-97f3-561f0643153a", label: "20" },
    { user_id: "23e3a0cc-588a-4a91-8709-0be31c89ce6e", label: "21" },
    { user_id: "8cb742cb-d04c-4714-b11a-a4ca54d7fd30", label: "22" },
+   { user_id: "8cb742cb-d04c-4714-b11a-a4ca54d7fd30", label: "23" },
 ];
 
 export default function Standings({
